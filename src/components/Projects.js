8113147c398d083e3137663dde7883f03037fccd
Criste Phillips <criste.phillips@yahoo.com>
2022-04-10 15:10:03 -0400

    pertinent info for projects selected to add to portfolio added as comments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,16 +13,48 @@
 
 // 6 projects
 
-// 1 Interactive Front-End Project (M8 Group Project)
+// 1 Interactive Front-End Project (M8 Group Project)   
+        // Title: Comics and Cocktails
+        // GitHub Repo: https://github.com/cphill11/comics-and-cocktails.git
+        // Not deployed to heroku.. deploy (?)
+        // comics-and-cocktails.jpg
+
 
 // 2  Node.js Challenge: Professional README Generator (M9)
+        // Title: README Generator
+        // GitHub Repo: https://github.com/cphill11/readme-generator.git
+        // Not deployed to heroku.. deploy (?)
+        // readme-generator.jpg
 
 // 3  Object-Oriented Programming Challenge: Team Profile Generator (M10)
+        // Title: Team Profile Generator
+        // GitHub Repo: https://github.com/cphill11/team-profile-generator.git
+        // Screencastify link: https://watch.screencastify.com/v/uRXPsHu7ADH433kBH6pT
+        // Not deployed to heroku.. deploy (?)
+        // team-profile-generator.jpg
 
 // 4  Express.js Challenge: Note Taker (M11)
+        // Title: Note Taker
+        // GitHub Repo: https://github.com/cphill11/note-taker.git
+        // Heroku Link: https://intense-eyrie-70687.herokuapp.com/
+        // note-taker.jpg
 
 // 5  SQL Challenge: Employee Tracker (M12)
+        // Title: Employee Tracker
+        // GitHub Repo: https://github.com/cphill11/employee-tracker.git
+        // Not deployed to heroku.. deploy (?)
+        // Sceencastify Link: https://drive.google.com/file/d/1SY0HslbB1ZDxtMdc_XAlGYdc4vmDv-Qp/view
+        // employee-tracker.jpg
 
 // 5 alternate Interactive Full-Stack Project (M16 Group Project)
+        // Title: Fetch
+        // GitHub Repo: https://github.com/cphill11/fetching.git
+        // Heroku link: https://intense-crag-43217.herokuapp.com/
+        // fetch.jpg
 
 // 6  NoSQL Challenge: Social Network API  (M18)
+        // Title: Social Network
+        // GitHub Repo: https://github.com/cphill11/social-network.git
+        // Sceencastify Link:: https://drive.google.com/file/d/13Uru265ko2wr6lN3sOSLv3wlVoAbHV5E/view
+        // Not deployed to heroku.. deploy (?)
+        // social-network.jpg
