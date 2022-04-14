fd2b850b9dbbff062d56655b4ce222ce07f0f6b9
Criste Phillips <criste.phillips@yahoo.com>
2022-04-13 22:50:13 -0400

    footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,29 @@
 import React from "react";
 
 function Footer() {
-  return <section>INSERT LINKS and format to be pretty</section>;
+  return (
+    <section className="my-5">
+      <li>
+        <a href="https://www.linkedin.com/in/cristephillips/" class="linkedIn">
+          linkedIn
+        </a>
+      </li>
+      <br></br>
+      <li>
+        <a href="https://github.com/cphill11" class="gitHub">
+          GitHub
+        </a>
+      </li>
+      <br></br>
+      <li>
+        <a
+          href="https://stackoverflow.com/users/17097977/criste-phillips"
+          class="stackOverflow">
+          Stack Overflow
+        </a>
+      </li>
+    </section>
+  );
 }
 
 export default Footer;
-
-// Footer displays text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow)
-// Linked in / github / stackoverflow
-// <li><a href="https://www.linkedin.com/in/cristephillips/" class="linkedIn">linkedin.com/in/cristephillips/</a></li>
-//<li><a href="https://github.com/cphill11" class="gitHub">GitHub</a></li>
-// <li><a href="https://stackoverflow.com/users/17097977/criste-phillips" class="stackOverflow">Stack Overflow</a></li>
