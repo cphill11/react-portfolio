d2b66b1314a7c9ff5ceadfb3b00250fc9f69aeb0
Criste Phillips <criste.phillips@yahoo.com>
2022-04-13 21:55:37 -0400

    nav updated with categories for Resume and Portfolio

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,50 @@
 // bar that we see; operates w/ in header that will conditionally render different sections of the portfolio
-import React from 'react';
+import React from "react";
 
 // import React, { useEffect } from 'react';
-// import { capitalizeFirstLetter } from '../utils/helpers';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
+  const categories = [
+    {
+      name: "Resume",
+      description: "Listing of skillsets useful for employment opportunities",
+    },
+    {
+      name: "Portfolio",
+      description: "Challenges and Projects completed during coding bootcamp",
+    },
+  ];
+
+  const handleClick = () => {
+    console.log("click handled");
+  };
   return (
-       <header>
-         <h2>
-           {/* <a href="/"></a> */}
-         </h2>
-         <nav>
-           <ul className="flex-row">
-             <li className="mx-2">
-               <a href="#about"> About Me</a>
-             </li>
-             <li>
-               <span>Contact</span>
-             </li>
-           </ul>
-         </nav>
-       </header>
-      );
-    }
-
-
-
-
+    <header data-testid="header" className="flex-row px-1">
+      <h2>{/* <a href="/"></a> */}</h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>Contact</span>
+          </li>
+          {
+            categories.map((category) => (
+              <li className="mx-1" key={category.name} >
+                <span onClick={() => { handleClick(); }}>
+                 {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Nav;
