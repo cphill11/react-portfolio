45ea98128707282c863891e8160fb49befd4bd34
Criste Phillips <criste.phillips@yahoo.com>
2022-04-17 20:14:34 -0400

    footer fixed to portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,56 +4,76 @@ import comicImg from "../assets/images/comics-and-cocktails.jpg";
 import teamImg from "../assets/images/team-profile-generator.jpg";
 import readImg from "../assets/images/readme-generator.jpg";
 import noteImg from "../assets/images/note-taker.jpg";
-import empImg from "../assets/images/employee-tracker.jpg";
 import fetchImg from "../assets/images/fetch.jpg";
 import socialImg from "../assets/images/social-network.jpg";
+import Footer from "./Footer";
 
 function Portfolio() {
   return (
     <section className="my-5" class="container-fluid">
-      <h1 id="portfolio" class="row">Portfolio Display</h1>
-  
-      <div class="col-6-md-auto">
-        {/* Interative Front-End Group Project */}
-        <Project title="Comics and Cocktails" image={comicImg} href="https://github.com/cphill11/comics-and-cocktails.git"/>
+      <h1 id="portfolio" class="row">
+        Portfolio Display
+      </h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-6-md-auto">
+            {/* Interative Front-End Group Project */}
+            <Project
+              title="Comics and Cocktails"
+              image={comicImg}
+              href="https://github.com/cphill11/comics-and-cocktails.git"
+            />
+          </div>
+
+          <div class="col-6-md-auto">
+            {/* Object-Oriented Programming Challenge */}
+            <Project
+              title="Team Profile Generator"
+              image={teamImg}
+              href="https://github.com/cphill11/team-profile-generator.git"
+            />
+          </div>
+
+          <div class="col-6-md-auto">
+            {/* Node.js Challenge */}
+            <Project
+              title="README Generator"
+              image={readImg}
+              href="https://github.com/cphill11/readme-generator.git"
+            />
+          </div>
+
+          <div class="col-6-md-auto">
+            {/* Express.js Challenge: Note Taker */}
+            <Project
+              title="Note Taker"
+              image={noteImg}
+              href="https://github.com/cphill11/note-taker.git"
+            />
+          </div>
+
+          <div class="col-6-md-auto">
+            {/* Interactive Full-Stack Group Project */}
+            <Project 
+              title="Fetch"
+              image={fetchImg}
+              href="https://github.com/cphill11/fetching.git" 
+              />
+          </div>
+
+          <div class="col-6-md-auto">
+            {/* NoSQL Social Network API */}
+            <Project
+              title="Social Network"
+              image={socialImg}
+              href="https://github.com/cphill11/social-network.git"
+            />
+          </div>
+        </div>
+       <Footer />
       </div>
-
-      <div class="col-6-md-auto">
-        {/* Object-Oriented Programming Challenge */}
-        <Project title="Team Profile Generator" image={teamImg} href="https://github.com/cphill11/team-profile-generator.git"/>
-        </div>
-
-        <div class="col-6-md-auto">
-        {/* Node.js Challenge */}
-        <Project title="README Generator" image={readImg} href="https://github.com/cphill11/readme-generator.git"/>
-        </div>
-
-        <div class="col-6-md-auto">
-        {/* Express.js Challenge: Note Taker */}
-        <Project title="Note Taker" image={noteImg} href="https://github.com/cphill11/note-taker.git"/>
-        </div>
-
-        <div class="col-6-md-auto">
-        {/* SQL Challenge */}
-        <Project title="Employee Tracker" image={empImg} href="https://github.com/cphill11/employee-tracker.git"/>
-        </div>
-
-        <div class="col-6-md-auto">
-        {/* Interactive Full-Stack Group Project */}
-        <Project title="Fetch" image={fetchImg} href="https://github.com/cphill11/fetching.git"/>
-        </div>
-
-        <div class="col-6-md-auto">
-        {/* NoSQL Social Network API */}
-        <Project title="Social Network" image={socialImg} href="https://github.com/cphill11/social-network.git"/>
-        </div>
-
     </section>
   );
 }
 
 export default Portfolio;
-
-// Display titled images of 6 of the developer's applications with links to both the deployed applications and the corresponding GitHub repository.
-
-// Loading the portfolio the first time displays the About Me title and section are selected by default.
