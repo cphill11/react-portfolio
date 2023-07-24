f61dd6f123a2888fc14e834f67bfc78f8b9cfc4d
Criste Phillips <criste.phillips@yahoo.com>
2023-07-24 17:33:40 -0400

    updated readme image profile with a recent sceenshot of the local app, removed extraneous images and files

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,7 @@
 import React from "react";
 import Project from "./Project";
-import comicImg from "../assets/images/comics-and-cocktails.jpg";
-import teamImg from "../assets/images/team-profile-generator.jpg";
 import keepingTallyImg from "../assets/images/keepingTally.jpg";
 import familyPetImg from "../assets/images/familyPets.jpg";
-import fetchImg from "../assets/images/fetch.jpg";
 import greatImg from "../assets/images/great-reads.jpg";
 import Footer from "./Footer";
 function Portfolio(props) {
@@ -24,12 +21,6 @@ function Portfolio(props) {
             image={familyPetImg}
             href="https://github.com/cphill11/family-pets/blob/main/README.md"
           />
-         <Project
-            title="Team Profile Generator"
-            subtitle="Object-Oriented Programming"
-            image={teamImg}
-            href="https://github.com/cphill11/team-profile-generator.git"
-          />
         </div>
         <div className="SplitPane-Mid">
           {" "}
@@ -39,27 +30,13 @@ function Portfolio(props) {
             subtitle="Html, CSS, JS"
             image={keepingTallyImg}
             href="https://github.com/cphill11/keeping-tally/blob/main/README.md"
-          />
-         
-         <Project
-            title="Comics and Cocktails"
-            subtitle="Full Stack Group Project"
-            image={comicImg}
-            href="https://github.com/cphill11/comics-and-cocktails.git"
-          />
-   
+          />        
+ 
         </div>
         <div className="SplitPane-Right">
           {" "}
-          {props.right}
-              
-          <Project
-            title="Fetch"
-            subtitle="Full Stack Group Project"
-            image={fetchImg}
-            href="https://github.com/cphill11/fetching.git"
-          />
-   
+          {props.right}  
+ 
           <Project
             title="Great Reads"
             subtitle="Full Stack Group Project"
