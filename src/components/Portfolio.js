5c44c1e8b1f2598ba0628f692ba40cc880d3510a
Criste Phillips <criste.phillips@yahoo.com>
2023-07-28 17:42:41 -0400

    updated with new project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from "react";
 import Project from "./Project";
 import keepingTallyImg from "../assets/images/keepingTally.jpg";
 import familyPetImg from "../assets/images/familyPets.jpg";
-// import greatImg from "../assets/images/great-reads.jpg";
+import randomPswdGenImg from "../assets/images/randomPswdGen.jpg";
 import Footer from "./Footer";
 function Portfolio(props) {
   return (
@@ -17,32 +17,32 @@ function Portfolio(props) {
         <div className="SplitPane-Left">   
           <Project
             title="Family Pets"
-            subtitle="Front-End Only"
+            subtitle="Social App"
             image={familyPetImg}
             href="https://github.com/cphill11/family-pets/blob/main/README.md"
           />
+
+          {" "}
+          {props.left}  
+           <Project
+            title="Random Password Generator"
+            subtitle="Interactive and User Friendly"
+            image={randomPswdGenImg}
+            href="https://github.com/cphill11/random-password-generator/blob/main/README.md"
+          />
+    
+
         </div>
         <div className="SplitPane-Right">
           {" "}
           {props.right}     
           <Project
             title="Keeping Tally"
-            subtitle="Html, CSS, JS"
+            subtitle="Interactive Lists"
             image={keepingTallyImg}
             href="https://github.com/cphill11/keeping-tally/blob/main/README.md"
           />        
         </div>
-        {/* <div className="SplitPane-Right">
-          {" "}
-          {props.right}  
- 
-          <Project
-            title="Great Reads"
-            subtitle="Full Stack Group Project"
-            image={greatImg}
-            href="https://github.com/cphill11/great-reads/blob/main/README.md"
-          />
-        </div> */}
       </div>
       </div>
       <Footer/>
