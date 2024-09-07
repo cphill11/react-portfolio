04b291b995ebc3965fcfe1a7ef712ae77361c7f5
Criste Phillips <criste.phillips@yahoo.com>
2024-09-07 13:46:17 -0400

    more robust media query template added

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import professionalImage from "../assets/images/professional-image.jpg";
 import islandImage from "../assets/images/nz.jpg";
 import pdf from "../assets/small/resume.pdf";
 import '../index.css';
-// import '../media-queries.css';
-// import '../header-footer-media-queries.css';
+import '../media-queries.css';
+import '../header-footer-media-queries.css';
 
 function About(props) {
   // JSX being returned to represent HTML in JS
